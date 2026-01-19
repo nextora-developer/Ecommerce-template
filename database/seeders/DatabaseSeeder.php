4515f29109121f463a2f01b3114840728c39e0af
mingxuanyap <mingxuanyap622@gmail.com>
2026-01-19 22:02:59 +0800

    Update seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -38,6 +38,8 @@ class DatabaseSeeder extends Seeder
         );
 
         $this->call([
+            CategorySeeder::class,
+            ProductSeeder::class,
             PaymentMethodSeeder::class,
             ShippingRateSeeder::class,
         ]);
